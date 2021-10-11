62a30f704efa30d0570c00cee74873810cbc3932
CobeñaCornejoRoberto <roberto_samp@outlook.com>
2021-10-10 20:47:23 -0500

    Agregar archivos de proyecto.

---
+++ RobertoCobeña3A9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RobertoCobeña3A9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.Write("Ingrese su número:");
+            num = int.Parse(Console.ReadLine());
+            Console.Write("Tabla del:" + num);
+            Console.Write("\n");
+                for (int i = 1; i <= 15; i++)
+                {
+                    Console.WriteLine(num + "x" + i + "=" + num * i);
+                }
+        }
+    }
+}
